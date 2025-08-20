b566b9c3fe825c05cdf7ac898dfbf4cf6eccff1a
guru-sahoo <gurusahoodev@gmail.com>
2025-08-20 13:56:05 +0530

    app.js comments

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 const mergeDBs = require("./mergeDB");
 
-mergeDBs("waGrpsAuto1", "waGrpsAuto");
+mergeDBs("waGrpsAuto1", "waGrpsAuto"); // Add source & destination db here
 
 app.listen(3000, () => console.log("Server running on port 3000"));
